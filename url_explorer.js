e15e1b4d708b378adafd7c1c4b7fdc02bd2cb627
Fortune-Ndlovu <fndlovu@redhat.com>
2024-11-02 23:39:14 +0000

    Create a URL object from a given URL string, extracts various components of the URL, and logs them to the console.

--- url_explorer.js
+++ url_explorer.js
@@ -1,1 +1,12 @@
 // Create an instance of the url by passing a url to it
+
+const url = new URL('https://www.someserver.com/not/a/path?param1=value1&param2=value2');
+
+console.log(`URL is: ${url.href}`);
+console.log(`Hostname: ${url.hostname}`);
+console.log(`Path: ${url.pathname}`);
+
+console.log(`Query string is: ${url.search}`);
+console.log(`Query parameters:`);
+Array.from(url.searchParams.entries())
+    .forEach((entry) => console.log(`\t- ${entry[0]} = ${entry[1]}`));
