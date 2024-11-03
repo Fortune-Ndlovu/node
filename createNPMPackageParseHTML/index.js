20807e8b2dbc46d1d44da770f449e53b5b79164b
Fortune-Ndlovu <fndlovu@redhat.com>
2024-11-03 11:58:08 +0000

    create index.js file and add the dependency and html structure

---
+++ createNPMPackageParseHTML/index.js
@@ -0,0 +1,16 @@
+const cheerio = require('cheerio');
+
+const html = `
+<html>
+    <head>
+        <title>Sample Page</title>
+    </head>
+    <body>
+        <p>This is a paragraph</p>
+        <div>
+            <p>This is a paragraph inside a div</p>
+        </div>
+        <button>Click me!</button>
+    </body>
+</html>
+`
